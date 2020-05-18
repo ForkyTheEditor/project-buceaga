5cb9a7a1e215c6b4ec455218ce1c6eb2841014f0
ForkyTheEditor <boohoochicken@gmail.com>
2020-05-19 02:50:01 +0300

    Small update on UI work
    After we get to a stable, working build, we will start using branches from now on.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //This is a singleton object, check for other instances and DESTROY them
+    private static GameManager instance;
+
+    private static GameObject _localPlayerInstance;
+    public static GameObject localPlayerInstance { get { return _localPlayerInstance; } }
+
+
+    //INSTANTIATE THE OBJECT
+    private void Awake()
+    {
+        //Check for other accidentally created instances and destroy them
+        if(instance != null && instance != this)
+        {
+            //You do not matter, kill yourself. bitch.
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+
+    public static void SetLocalPlayer(GameObject newPlayer)
+    {
+        _localPlayerInstance = newPlayer;
+    }
+
+
+}
--- Assets/Scripts/UI/UIPlayer.cs
+++ Assets/Scripts/UI/UIPlayer.cs
@@ -4,8 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class UIResourceAmount : MonoBehaviour
+public class UIPlayer : MonoBehaviour
 {
+
+    //The stats of the player to use (not necessarily the local player, for instance
+    //in the future we might want the player to be able to click on other units and see their inventory
+    // like in DOTA for instance)
+    private CharacterStats playerStats;
+
     //TODO: Find how to get the player reference in order to display their inventory
     [SerializeField] private ResourceInventory resourceInv;
     private TextMeshProUGUI resourceText;
@@ -13,13 +19,20 @@ public class UIResourceAmount : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Get instance of the local player for starters!
+        playerStats = GameManager.localPlayerInstance.GetComponent<CharacterStats>();
+        if(playerStats == null)
+        {
+            Debug.LogError("Local player instance couldn't be found!",this);
+        }
         resourceText = gameObject.GetComponent<TextMeshProUGUI>();
-        resourceInv = gameObject.GetComponentInParent<ResourceInventory>();
+        resourceInv = playerStats.GetComponent<ResourceInventory>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        
         //Check for errors
         if (resourceText == null || resourceInv == null)
         {
@@ -29,16 +42,13 @@ public class UIResourceAmount : MonoBehaviour
         resourceText.text = "";
 
         //Show all resource types
-        foreach(ResourceTypes rt in Enum.GetValues(typeof(ResourceTypes)))
+        foreach (ResourceTypes rt in Enum.GetValues(typeof(ResourceTypes)))
         {
             string resourceName = rt.ToString();
             int playerResourceAmount = resourceInv.GetResource(rt);
 
-
             resourceText.text += resourceName + ":" + playerResourceAmount.ToString();
 
         }
-
-
     }
 }
