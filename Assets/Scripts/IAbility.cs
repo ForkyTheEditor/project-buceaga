57b6dd45a77458a8377e2676b0e2d65d720fb5c3
ForkyTheEditor <boohoochicken@gmail.com>
2020-08-16 04:52:05 +0300

    Started work on the ability system

---
+++ Assets/Scripts/IAbility.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAbility
+{
+    /// <summary>
+    /// Call to use the ability;
+    /// </summary>
+    void CastAbility();
+
+}
---
+++ Assets/Scripts/Player/PlayerAbilityMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilityMotor : MonoBehaviour
+{
+
+    List<IAbility> abilityList = new List<IAbility>();
+
+    /// <summary>
+    /// Casts the ability assigned to the parameter hotkey
+    /// </summary>
+    /// <param name="hotkey"></param>
+    public void CastAbility(KeyCode hotkey)
+    {
+
+    }
+
+}
