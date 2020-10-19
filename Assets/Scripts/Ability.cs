dd27eb6f6725680e00d0afdc63a440273edfadf6
ForkyTheEditor <20324370+ForkyTheEditor@users.noreply.github.com>
2020-10-19 22:45:56 +0300

    Started work on ability system. Fixed navmesh not dynamic.

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Ability")]
+public class Ability : ScriptableObject
+{
+
+    public new string name;
+
+    
+}
