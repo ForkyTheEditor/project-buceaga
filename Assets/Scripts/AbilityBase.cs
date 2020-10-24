cdf7a5f3a288cd3bfa0233d8af0a0d7b9c2bb3b1
ForkyTheEditor <boohoochicken@gmail.com>
2020-10-24 15:48:57 +0300

    More work on ability system

---
+++ Assets/Scripts/AbilityBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abstract superclass of all abilities in game. Inherit from this when creating a new ability.
+/// </summary>
+public abstract class AbilityBase : ScriptableObject
+{
+    public new string name;
+    [TextArea]
+    public string description;
+    public GameObject abilityGO;
+    
+    
+
+    
+    
+}
