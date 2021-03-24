7103e4614f907a9240ec7a8b270d01a4cfece866
Alex Forky <20324370+ForkyTheEditor@users.noreply.github.com>
2021-03-25 01:05:39 +0200

    Ported to cinemachine

--- Assets/Scripts/Player/PlayerCameraController.cs
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerCameraController : MonoBehaviour
 {
+    //TODO: Add zoom function
 
     [SerializeField]
-    private float moveSpeed = 20f;
-
+    private float panSpeed = 20f;
     [SerializeField]
+    private float zoomSpeed = 10f;
+    [SerializeField]
+    private float zoomInMax = 5f;
+
+   [SerializeField]
     private int borderThickness = 1;
+   
 
     private GameObject localPlayer = null;
 
+    private CinemachineInputProvider inputProvider = null;
+    private CinemachineVirtualCamera freeCamera = null;
+    private Transform camTransform = null;
+
+
+
     private void Awake()
     {
+
+        inputProvider = GetComponent<CinemachineInputProvider>();
+        freeCamera = GetComponent<CinemachineVirtualCamera>();
+        camTransform = freeCamera.VirtualCameraGameObject.transform;
+
         //Find the player object
         StartCoroutine(InitializePlayer());
     }
@@ -29,36 +47,52 @@ public class PlayerCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //MOBA style camera
-        FreeCameraMove();
-       
-    }
+        float x = inputProvider.GetAxisValue(0);
+        float y = inputProvider.GetAxisValue(1);
+        float z = inputProvider.GetAxisValue(2);
 
-    
-
-    void FreeCameraMove()
-    {
-        //Check if the application is actually focused, so you can't Alt+Tab and still move the camera 
-        if (Application.isFocused)
+        if (x != 0 | y != 0)
         {
-            //Move the actual position of the camera if the player touches the edges of the screen
-            if (Mouse.current.position.ReadValue().y >= Screen.height - borderThickness)
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-            }
-            if (Mouse.current.position.ReadValue().y <= 0 + borderThickness)
-            {
-                transform.Translate(Vector3.back * Time.deltaTime * moveSpeed, Space.World);
-            }
-            if (Mouse.current.position.ReadValue().x >= Screen.width - borderThickness)
-            {
-                transform.Translate(Vector3.right * Time.deltaTime * moveSpeed, Space.World);
-            }
-            if (Mouse.current.position.ReadValue().x <= 0 + borderThickness)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * moveSpeed, Space.World);
-            }
-
+            PanScreen(x, y);
         }
+
     }
+
+    public Vector2 FindPanDirection(float x, float y)
+    {
+        Vector2 retVect = Vector2.zero;
+        if (y >= Screen.height - borderThickness)
+        {
+            retVect.y += 1;
+        }
+        else if (y <= 0 + borderThickness)
+        {
+            retVect.y -= 1;
+        }
+        if (x >= Screen.width - borderThickness)
+        {
+            retVect.x += 1;
+        }
+        else if (x <= 0 + borderThickness)
+        {
+            retVect.x -= 1;
+        }
+
+        return retVect;
+    }
+
+    public void PanScreen(float x, float y)
+    {
+        
+        if (!Application.isFocused)
+        {
+            return;
+        }
+
+        Vector2 dir = FindPanDirection(x, y);
+        //Make sure you cast the dir to a vector3 so that you move on the X and Z axes.
+        camTransform.position = Vector3.MoveTowards(camTransform.position, camTransform.position + new Vector3(dir.x, 0, dir.y), panSpeed * Time.deltaTime);
+        
+    }
+
 }
